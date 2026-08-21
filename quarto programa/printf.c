39e4e10c736cd4040d77ac1ed80ed1cb8d70f830
CardosoMiguel <cardosomiguel.contatopro@gmail.com>
2026-08-21 20:04:10 -0300

    Add files via upload

---
+++ quarto programa/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    printf("oi, \ntudo \nbem?");
+    printf("\t oiiii\n");
+    printf("C é\n \"SUPER\" \nFacil! ");
+}
---
+++ quinto programa/pratica1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("------------------");
+    char nome[10];
+    printf("\nDigite seu nome: ");
+    scanf("%s", &nome);
+    printf("\nO seu nome é %s\n", nome);
+    printf("\n------------------");
+
+}
